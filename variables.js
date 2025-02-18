7e342994492c081be22a7228e71d39fe3bee075e
Saranya <jsaran.bj@gmail.com>
2025-02-18 12:03:13 +0100

    Javascript DOm manipulation

--- variables.js
+++ variables.js
@@ -1,2 +1,7 @@
-const body = document.body.classList;
-console.log({body})
+const body = document.body;
+const h1 = document.createElement('h1');
+h1.innerText = 'welcome !';
+body.append(h1)
+console.log({h1})
+
+ 
